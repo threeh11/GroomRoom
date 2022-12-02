716017eb8963dbd8a255e39dcc65cb393e56c1ad
threeh <lemass338@mail.ru>
2022-12-02 09:50:58 +0300

    add files routes

--- routes/profile.php
+++ routes/profile.php
@@ -1,13 +1,7 @@
 <?php
 
-use App\Http\Controllers\PagesController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', [PagesController::class, 'index'])->name('index');
-Route::get('/aplications', [PagesController::class, 'Applications'])->name('Applications');
-Route::get('/all_applications', [PagesController::class, 'allAplications'])->name('allAplications');
-Route::get('/profile', [PagesController::class, 'profile'])->name('profile');
 
 Route::get('/profile/my_pet', [ProfileController::class , 'myPet'])->name('myPet');
 Route::post('/profile/my_pet/add_pet', [ProfileController::class, 'addPet'])->name('addPet');
